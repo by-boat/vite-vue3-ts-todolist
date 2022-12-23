b663a20facd2ac73d00ad57d82954986b36af7f9
Zhisai Wu 吴之赛(WTCCN HO IT-IT Partner) <Zhisai.Wu@watsons.com.cn>
2022-12-23 16:58:18 +0800

    测试githook

---
+++ test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+// 读取data.json文件的数据，操作一番后写入到新的 newdata.json文件中
+fs.readFile(path.join(__dirname, './data.json'), (err, data) => {
+  if (err) {
+    return console.log(err)
+  }
+  // let data_ = JSON.parse(data.toString())
+  let obj = {};
+  obj.a = 1;
+  obj.b = 2;
+
+  fs.writeFile(path.join(__dirname, './data.json'), JSON.stringify(obj), (err) => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('写入成功')
+  })
+})
